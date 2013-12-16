cc43694aba2d3e1a4c3230d525b56c7b1c6ec9b5
Chris Greene <archgoon@gmail.com>
2013-12-16 00:36:53 -0800

    ROSLAIND-5 Remove parse_codons.py
    https://github.com/cwgreene/rosalind/issues/5
    Fixes #5

--- solutions/parse_codons.py
+++
@@ -1,9 +0,0 @@
-import sys
-from rosalind import take
-def parse_codons(file):
-    result = {}
-    for line in file.readlines():
-        for codon, acid in takeby(2, line.split()):
-            result[codon] = acid
-    print 'codons = '+str(result).replace(",",",\n")
-parse_codons(open(sys.argv[1]))

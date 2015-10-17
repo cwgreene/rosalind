fe3a29301c61ff6d70da9bd61a41ad79b9591a40
Chris Greene <archgoon@gmail.com>
2015-10-16 22:35:13 -0700

    ROSALIND-32 Fix commit message script
    Forgot a '%' in the format script. Not entirely sure
    why the pre-push script allowed it; possibly using the wrong
    local pre-push script.
    
    Fixes https://github.com/cwgreene/rosalind/issues/32

--- hooks/format-commit.sh
+++ hooks/format-commit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git commit --amend -m "$(git show -s HEAD --format="%sn%n%b" | hooks/format-commit-message.py)"
+git commit --amend -m "$(git show -s HEAD --format="%s%n%n%b" | hooks/format-commit-message.py)"

791aa616fd70d54cebbc8e035d6a4f892795a5e9
Chris Greene <archgoon@gmail.com>
2016-02-25 21:54:35 -0800

    ROSALIND-34 Begin getting Long going.
    Writing hacky overlap function. really tired and not thinking clearly
    today.
    
    refs #34

--- solutions/long.py
+++ solutions/long.py
@@ -1,3 +1,14 @@
 from rosalind import simplereadfasta
+from rosalind import annotations
+
+def compute_overlap(string1, string2):
+    string1, string2 = (
+            min(string1, string2, key=len),
+            max(string1, string2, key=len))
+    for shift in range(len(string2)+len(string1)):
+        j2 = min(length(string2), shift)
+        i2 = max(i-length(string1), 0)
+        j1 = min(length(string1), 
+        
 
 strings = simplereadfasta()

f8d6cee0716c93fe451731dd6e81b3535b9ca5c0
Chris Greene <archgoon@gmail.com>
2016-02-28 23:41:01 -0800

    ROSALIND-34 Initial Stab at Long
    Okay, so this passes the test case, next to try it on real input.

--- solutions/long.py
+++ solutions/long.py
@@ -2,13 +2,38 @@ from rosalind import simplereadfasta
 from rosalind import annotations
 
 def compute_overlap(string1, string2):
-    string1, string2 = (
-            min(string1, string2, key=len),
-            max(string1, string2, key=len))
+    if len(string2) < len(string1):
+        string1, string2 = string2, string1
+    max_overlap = 0
+    length1 = len(string1)
+    length2 = len(string2)
+    overlap_string = string1 + string2
     for shift in range(len(string2)+len(string1)):
-        j2 = min(length(string2), shift)
-        i2 = max(i-length(string1), 0)
-        j1 = min(length(string1), 
-        
+        i1 = max(length1 - shift, 0)
+        j1 = min(length1, length1 - (shift - length2))
+        j2 = min(shift, length2)
+        i2 = max(0, shift-length1)
+        overlap = 0
+        if string1[i1:j1] == string2[i2:j2]:
+            overlap = j1-i1
+            if shift < length1:
+                overlap_string = string1[:length1-shift] + string2
+            else:
+                overlap_string = string2 + string1[length1-shift:]
+    return max_overlap, overlap_string
 
-strings = simplereadfasta()
+strings_all = set(simplereadfasta())
+strings = set(strings_all)
+while len(strings) > 1:
+    string = strings.pop()
+    max_overlap = ((-1, ""), "")
+    for other in strings:
+        max_overlap = max(
+                (compute_overlap(other, string), other),
+                max_overlap,
+                key = lambda x:x[0][0])
+    (value, merged), other = max_overlap
+    strings.remove(other)
+    strings.add(merged)
+final = strings.pop()
+print final
